768801d30ab5d02d18bfbfd1a815664c75c3db57
Angelo <skyhowl1153@gmail.com>
2019-01-16 22:44:27 +0800

    added new templates, views, forms

--- polls/views.py
+++ polls/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Question
+from .forms import QuestionModelForm
 
 # Create your views here.
 
@@ -13,3 +14,22 @@ def index(request):
 
 def help(request):
     return HttpResponse('This is the help page')
+
+
+def detail(request, question_id):
+    context = {}
+    context['question'] = Question.objects.get(id=question_id)
+    return render(request, 'details.html', context)
+
+def update(request, question_id):
+    if request.method == 'POST':
+        #question = Question.objects.get(id=question_id)
+        form = QuestionModelForm(request.POST, instance=question)
+        if form.is_valid():
+            form.save()
+        return HttpResponse('Question Updated')
+    else:
+        question = Question.objects.get(id=question_id)
+        context = {}
+        context['form'] = QuestionModelForm(instance=question)
+        return render(request, 'update.html', context)
